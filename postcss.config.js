6831d10e8c3c32307d57f8d2f6fec16e3ba4daa0
Oleg V <oleg.vintonuk@gmail.com>
2021-01-26 00:19:39 +0200

    add postCSS css-mquery-packer

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: [
-    require('autoprefixer'),
     require('postcss-combine-duplicated-selectors'),
+    require("css-mquery-packer"),
+    require('autoprefixer'),
   ],
 };
